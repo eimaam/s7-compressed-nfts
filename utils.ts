28ac6b3d473d9def60737b959231cdb728c583c2
eimaam <imamddahir@gmail.com>
2024-09-27 05:53:27 +0100

    FEAT: Add utility functions for generating and retrieving keypairs

---
+++ utils.ts
@@ -0,0 +1,36 @@
+import { Connection, Keypair } from "@solana/web3.js";
+import fs from "fs";
+
+export const NFT_IMAGE =
+  "https://drive.google.com/file/d/1fNcuChDDSdUjMp7bClVaSkBQ4mjk3yMN/view?usp=sharing";
+
+// Generate keypair and store it in a file keypair.json
+// aidropped sols to the keypair address to allow for umi transactions
+const generateSigner = async () => {
+  const keypair = Keypair.generate();
+  const keypairData = {
+    secretKey: Array.from(keypair.secretKey),
+    publicKey: keypair.publicKey.toBase58(),
+  };
+  fs.writeFileSync("keypair.json", JSON.stringify(keypairData));
+  return keypair;
+};
+
+export const getPublicKey = async () => {
+  const keypairData = JSON.parse(
+    fs.readFileSync("keypair.json", "utf-8").toString()
+  );
+  const publicKey = keypairData.publicKey;
+  return publicKey;
+};
+
+export const getSecretKey = () => {
+  const keypairData = JSON.parse(fs.readFileSync("keypair.json", "utf-8"));
+  const secretKey = new Uint8Array(keypairData.secretKey);
+  return secretKey;
+};
+
+export const getKeypair = () => {
+  const secretKey = getSecretKey();
+  return Keypair.fromSecretKey(secretKey);
+};
